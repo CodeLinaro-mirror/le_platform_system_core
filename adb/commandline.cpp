460141cfbc9e27fa3f164a076d83277e0e30c731
Josh Gao <jmgao@google.com>
2015-11-20 15:37:31 -0800

    adb: shell: add -n flag to not read from stdin.
    Shell scripts of the following form do not work properly with adb:
        echo "foo\nbar\nbaz" | {
            read FOO
            while [ "$FOO" != "" ]; do
                adb shell echo $FOO
                read FOO
            done
        }
    The first run of adb shell will consume all of the contents of stdin,
    causing the loop to immediately end. ssh solves this by providing a -n
    flag that causes it to not read from stdin. This commit adds the same.
    
    Bug: http://b/25817224
    Change-Id: Id74ca62ef520bcf03678b50f4bf203916fd81038
    (cherry picked from commit 7d586073609723cb2f6ed37de0ad1a7996e621ae)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -110,9 +110,10 @@ static void help() {
         "  adb sync [ <directory> ]     - copy host->device only if changed\n"
         "                                 (-l means list but don't copy)\n"
         "                                 (see 'adb help all')\n"
-        "  adb shell [-e escape] [-Tt] [-x] [command]\n"
+        "  adb shell [-e escape] [-n] [-Tt] [-x] [command]\n"
         "                               - run remote shell command (interactive shell if no command given)\n"
         "                                 (-e: choose escape character, or \"none\"; default '~')\n"
+        "                                 (-n: don't read from stdin)\n"
         "                                 (-T: disable PTY allocation)\n"
         "                                 (-t: force PTY allocation)\n"
         "                                 (-x: disable remote exit codes and stdout/stderr separation)\n"
@@ -1274,6 +1275,11 @@ int adb_commandline(int argc, const char **argv) {
                 ++argv;
             } else if (!strcmp(argv[0], "-x")) {
                 use_shell_protocol = false;
+                --argc;
+                ++argv;
+            } else if (!strcmp(argv[0], "-n")) {
+                close_stdin();
+
                 --argc;
                 ++argv;
             } else {

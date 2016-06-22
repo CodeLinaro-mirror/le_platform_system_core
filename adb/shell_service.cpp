2b2009597c4bf301b74e0346f06d793dea731f6d
Josh Gao <jmgao@google.com>
2016-06-22 15:57:12 -0700

    adb: don't dup local socket fd.
    SIGHUP handling depends on the local socket fd being fully closed in
    order to trigger an event on its counterpart. Release the local socket
    fd inside of Subprocess when returning it to ensure this.
    
    Bug: http://b/29565233
    Change-Id: I13b17bcddf0b396a5f4880f9e410fbbf24d9370d
    (cherry picked from commit 42afe2033fddc7c43ccfeb5f20defe992d0bc1a2)

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -184,7 +184,7 @@ class Subprocess {
 
     const std::string& command() const { return command_; }
 
-    int local_socket_fd() const { return local_socket_sfd_.fd(); }
+    int ReleaseLocalSocket() { return local_socket_sfd_.Release(); }
 
     pid_t pid() const { return pid_; }
 
@@ -433,7 +433,7 @@ void* Subprocess::ThreadHandler(void* userdata) {
     Subprocess* subprocess = reinterpret_cast<Subprocess*>(userdata);
 
     adb_thread_setname(android::base::StringPrintf(
-            "shell srvc %d", subprocess->local_socket_fd()));
+            "shell srvc %d", subprocess->pid()));
 
     subprocess->PassDataStreams();
 
@@ -746,7 +746,8 @@ int StartSubprocess(const char* name, const char* terminal_type,
         return ReportError(protocol, error);
     }
 
-    D("subprocess creation successful: local_socket_fd=%d, pid=%d",
-      subprocess->local_socket_fd(), subprocess->pid());
-    return subprocess->local_socket_fd();
+    int local_socket = subprocess->ReleaseLocalSocket();
+    D("subprocess creation successful: local_socket_fd=%d, pid=%d", local_socket, subprocess->pid());
+
+    return local_socket;
 }

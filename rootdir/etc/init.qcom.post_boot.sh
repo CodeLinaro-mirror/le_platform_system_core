4438f1972322e7ba25bdc0ca9013b5658e804b2c
Liu Jianhua <jianhl@codeaurora.org>
2021-08-18 13:47:15 +0530

    post_boot: Change the GPU max_pwrlevel of SA6145
    Change the max frequency from 435MHz to 500MHz
    
    Change-Id: I2c9dc975660d0ac3d210641706c28debc167cb3e
    Signed-off-by: Liu Jianhua <jianhl@codeaurora.org>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -210,7 +210,7 @@ configure_sa6155_sku_parameters() {
         echo 1017600000 > /sys/class/devfreq/soc\:qcom,cpu0-cpu-l3-lat/max_freq
         echo 940800000 > /sys/class/devfreq/soc\:qcom,cpu6-cpu-l3-lat/min_freq
         echo 1017600000 > /sys/class/devfreq/soc\:qcom,cpu6-cpu-l3-lat/max_freq
-        echo 3 > /sys/class/kgsl/kgsl-3d0/max_pwrlevel
+        echo 0 > /sys/class/kgsl/kgsl-3d0/max_pwrlevel
         echo 1016 > /sys/devices/platform/soc/soc:aop-set-ddr-freq/set_ddr_capped_freq
     elif [ $feature_id == 5 ]; then
         echo "SKU Configured : SA6150"

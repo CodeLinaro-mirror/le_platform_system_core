4d5cb3e0ed38b818fbfd73b922c06709beb496d2
Ankit Khasa <quic_akhasa@quicinc.com>
2022-04-04 13:31:58 +0530

    system-core: banned api changes
    Changed banned api with appropriate alternatives
    sprintf()    :     snprintf()
    strcpy()     :     strlcpy()
    strncpy()    :     strlcpy()
    
    Change-Id: I0500b84673752325ac21abb27ff5f6a06b864d0c

--- powerapp/powerapp.c
+++ powerapp/powerapp.c
@@ -370,7 +370,7 @@ void suspend_or_resume(void)
    {
      if (suspend == 1)
      {
-       strcpy (buf, SUSPEND_STRING);
+       strlcpy(buf, SUSPEND_STRING, sizeof(buf));
        errno = 0;
        if (write(fd, buf, strlen(buf)) == -1)
        {

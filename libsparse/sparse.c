77f2f17a3724a364b82e692dafd7a425a91b7012
Raghuvar S <quic_raghuvar@quicinc.com>
2022-09-13 11:13:07 +0530

    Add APIs sparse_file_foreach_chunk and sparse_file_block_size
    Define functions sparse_file_foreach_chunk and
    sparse_file_block_size as part of libsparse which
    are required to support generating sparsed images
    with f2fs file system using make_f2fs tool.
    
    Change-Id: I04ad5fcc877569be368a59e8ca6ce747818e92ca

--- libsparse/sparse.c
+++ libsparse/sparse.c
@@ -46,6 +46,19 @@ struct sparse_file *sparse_file_new(unsigned int block_size, int64_t len)
 	return s;
 }
 
+struct chunk_data {
+  void* priv;
+  unsigned int block;
+  unsigned int nr_blocks;
+  int (*write)(void* priv, const void* data, size_t len, unsigned int block, unsigned int nr_blocks);
+};
+
+static int foreach_chunk_write(void* priv, const void* data, size_t len) {
+  struct chunk_data* chk = (struct chunk_data*)priv;
+
+  return chk->write(chk->priv, data, len, chk->block, chk->nr_blocks);
+}
+
 void sparse_file_destroy(struct sparse_file *s)
 {
 	backed_block_list_destroy(s->backed_block_list);
@@ -199,6 +212,37 @@ int sparse_file_callback(struct sparse_file *s, bool sparse, bool crc,
 	return ret;
 }
 
+int sparse_file_foreach_chunk(struct sparse_file* s, bool sparse, bool crc,
+                              int (*write)(void* priv, const void* data, size_t len,
+                                           unsigned int block, unsigned int nr_blocks),
+                              void* priv) {
+  int ret = 0;
+  int chunks;
+  struct chunk_data chk;
+  struct output_file* out;
+  struct backed_block* bb;
+
+  chk.priv = priv;
+  chk.write = write;
+  chk.block = chk.nr_blocks = 0;
+  chunks = sparse_count_chunks(s);
+  out = output_file_open_callback(foreach_chunk_write, &chk, s->block_size, s->len, false, sparse,
+                                  chunks, crc);
+
+  if (!out) return -ENOMEM;
+
+  for (bb = backed_block_iter_new(s->backed_block_list); bb; bb = backed_block_iter_next(bb)) {
+    chk.block = backed_block_block(bb);
+    chk.nr_blocks = (backed_block_len(bb) - 1) / s->block_size + 1;
+    ret = sparse_file_write_block(out, bb);
+    if (ret) return ret;
+  }
+
+  output_file_close(out);
+
+  return ret;
+}
+
 static int out_counter_write(void *priv, const void *data __unused, int len)
 {
 	int64_t *count = priv;
@@ -228,6 +272,10 @@ int64_t sparse_file_len(struct sparse_file *s, bool sparse, bool crc)
 	}
 
 	return count;
+}
+
+unsigned int sparse_file_block_size(struct sparse_file* s) {
+  return s->block_size;
 }
 
 static struct backed_block *move_chunks_up_to_len(struct sparse_file *from,

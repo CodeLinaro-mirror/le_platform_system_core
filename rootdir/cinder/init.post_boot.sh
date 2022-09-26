a686dbc795458049cad74a7edaeea31deb1fc4d8
quic_ysaispur <quic_ysaispur@quicinc.com>
2022-09-26 02:28:45 -0700

    Revert "Revert "post_boot: cinder: Make CPU governor schedutil""
    This reverts commit b9e86a1890b191bf153adad614c8016a6441feb1.
    
    Change-Id: I667542e79a5867679faff086fc3c62ca7e1e64bd

--- rootdir/cinder/init.post_boot.sh
+++ rootdir/cinder/init.post_boot.sh
@@ -31,4 +31,4 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Empty file
+echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor

32da507c5f2c9d1e580a2a56acdddf4d3cc314a6
Chitti Babu Theegala <quic_ctheegal@quicinc.com>
2023-02-06 19:08:56 +0530

    initial post-boot settings
    Change-Id: I93483ce5cbd3f1199bb19819a9b1b65bacfbfed4

--- rootdir/waipio/init.post_boot.sh
+++ rootdir/waipio/init.post_boot.sh
@@ -119,11 +119,21 @@ fi
 
 case "$target" in
     "waipio")
+    rev=`cat /sys/devices/soc0/revision`
 
-    # Make unbound workqueue not run on cpu0, since all irqs are
-    # handled by cpu0 as default, it will preempt the workqueue if
-    # the workqueue also run on cpu0, and the latency is out of control.
-    echo fe > /sys/devices/virtual/workqueue/cpumask
+    # Core control parameters for gold
+    echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+    echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+    echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
+    echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
+    echo 3 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
+
+    # Core control parameters for gold+
+    echo 0 > /sys/devices/system/cpu/cpu7/core_ctl/min_cpus
+    echo 60 > /sys/devices/system/cpu/cpu7/core_ctl/busy_up_thres
+    echo 30 > /sys/devices/system/cpu/cpu7/core_ctl/busy_down_thres
+    echo 100 > /sys/devices/system/cpu/cpu7/core_ctl/offline_delay_ms
+    echo 1 > /sys/devices/system/cpu/cpu7/core_ctl/task_thres
 
     # Controls how many more tasks should be eligible to run on gold CPUs
     # w.r.t number of gold CPUs available to trigger assist (max number of
@@ -133,16 +143,28 @@ case "$target" in
     # Setting to 1 by default which means there should be at least
     # 4 tasks eligible to run on gold cluster (tasks running on gold cores
     # plus misfit tasks on silver cores) to trigger assitance from gold+.
+    echo 1 > /sys/devices/system/cpu/cpu7/core_ctl/nr_prev_assist_thresh
 
     # Disable Core control on silver
     echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
 
     # Setting b.L scheduler parameters
-    echo 85 > /proc/sys/walt/sched_group_downmigrate
+    echo 95 95 > /proc/sys/walt/sched_upmigrate
+    echo 85 85 > /proc/sys/walt/sched_downmigrate
     echo 100 > /proc/sys/walt/sched_group_upmigrate
+    echo 85 > /proc/sys/walt/sched_group_downmigrate
     echo 1 > /proc/sys/walt/sched_walt_rotate_big_tasks
-    echo 0 > /proc/sys/walt/sched_coloc_busy_hysteresis_enable_cpus
+    echo 400000000 > /proc/sys/walt/sched_coloc_downmigrate_ns
+    echo 39000000 39000000 39000000 39000000 39000000 39000000 39000000 5000000 > /proc/sys/walt/sched_coloc_busy_hyst_cpu_ns
+    echo 240 > /proc/sys/walt/sched_coloc_busy_hysteresis_enable_cpus
+    echo 10 10 10 10 10 10 10 95 > /proc/sys/walt/sched_coloc_busy_hyst_cpu_busy_pct
+    echo 5000000 5000000 5000000 5000000 5000000 5000000 5000000 2000000 > /proc/sys/walt/sched_util_busy_hyst_cpu_ns
     echo 255 > /proc/sys/walt/sched_util_busy_hysteresis_enable_cpus
+    echo 15 15 15 15 15 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
+
+    # set the threshold for low latency task boost feature which prioritize
+    # binder activity tasks
+    echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
     # cpuset parameters
     echo 0-3 > /dev/cpuset/background/cpus
@@ -151,26 +173,50 @@ case "$target" in
     # Turn off scheduler boost at the end
     echo 0 > /proc/sys/walt/sched_boost
 
+    # Reset the RT boost, which is 1024 (max) by default.
+    echo 0 > /proc/sys/kernel/sched_util_clamp_min_rt_default
+
     # configure governor settings for silver cluster
     echo "walt" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/down_rate_limit_us
     echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/up_rate_limit_us
-    echo 940800 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
-    echo 691200 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-    echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/pl
+    if [ $rev == "1.0" ]; then
+	    echo 1190400 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
+    else
+	    echo 1267200 > /sys/devices/system/cpu/cpufreq/policy0/walt/hispeed_freq
+    fi
+    echo 614400 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+    echo 1 > /sys/devices/system/cpu/cpufreq/policy0/walt/pl
 
     # configure input boost settings
-    echo 1094400 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
+    if [ $rev == "1.0" ]; then
+	    echo 1382800 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
+    else
+	    echo 1171200 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
+    fi
     echo 120 > /proc/sys/walt/input_boost/input_boost_ms
 
-    # colocation V3 settings
-    echo 614400 > /sys/devices/system/cpu/cpufreq/policy0/walt/rtg_boost_freq
-    echo 51 > /proc/sys/walt/sched_min_task_util_for_boost
-    echo 35 > /proc/sys/walt/sched_min_task_util_for_colocation
-    echo 20000000 > /proc/sys/walt/sched_task_unfilter_period
+    # configure governor settings for gold cluster
+    echo "walt" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+    echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/down_rate_limit_us
+    echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/up_rate_limit_us
+    if [ $rev == "1.0" ]; then
+	    echo 1497600 > /sys/devices/system/cpu/cpufreq/policy4/walt/hispeed_freq
+    else
+	    echo 1555200 > /sys/devices/system/cpu/cpufreq/policy4/walt/hispeed_freq
+    fi
+    echo 1 > /sys/devices/system/cpu/cpufreq/policy4/walt/pl
 
-    # Enable conservative pl
-    echo 1 > /proc/sys/walt/sched_conservative_pl
+    # configure governor settings for gold+ cluster
+    echo "walt" > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
+    echo 0 > /sys/devices/system/cpu/cpufreq/policy7/walt/down_rate_limit_us
+    echo 0 > /sys/devices/system/cpu/cpufreq/policy7/walt/up_rate_limit_us
+    if [ $rev == "1.0" ]; then
+	echo 1536000 > /sys/devices/system/cpu/cpufreq/policy7/walt/hispeed_freq
+    else
+	echo 1728000 > /sys/devices/system/cpu/cpufreq/policy7/walt/hispeed_freq
+    fi
+    echo 1 > /sys/devices/system/cpu/cpufreq/policy7/walt/pl
 
     # configure bus-dcvs
     bus_dcvs="/sys/devices/system/cpu/bus_dcvs"
@@ -186,29 +232,72 @@ case "$target" in
         echo 4 > $llccbw/sample_ms
         echo 68 > $llccbw/io_percent
         echo 20 > $llccbw/hist_memory
-        echo 80 > $llccbw/down_thres
+        echo 10 > $llccbw/hyst_length
+        echo 30 > $llccbw/down_thres
         echo 0 > $llccbw/guard_band_mbps
         echo 250 > $llccbw/up_scale
         echo 1600 > $llccbw/idle_mbps
-        echo 40 > $llccbw/window_ms
+        echo 806000 > $llccbw/max_freq
+	echo 40 > $llccbw/window_ms
     done
 
     for ddrbw in $bus_dcvs/DDR/*bwmon-ddr
     do
-        echo "1144 1720 2086 2929 3879 5931 6515 8136" > $ddrbw/mbps_zones
+        echo "1720 2086 2929 3879 6515 7980 12191" > $ddrbw/mbps_zones
         echo 4 > $ddrbw/sample_ms
-        echo 68 > $ddrbw/io_percent
+        echo 80 > $ddrbw/io_percent
         echo 20 > $ddrbw/hist_memory
-        echo 80 > $ddrbw/down_thres
+        echo 10 > $ddrbw/hyst_length
+        echo 30 > $ddrbw/down_thres
         echo 0 > $ddrbw/guard_band_mbps
         echo 250 > $ddrbw/up_scale
         echo 1600 > $ddrbw/idle_mbps
-        echo 48 > $ddrbw/window_ms
+	echo 2092000 > $ddrbw/max_freq
+        echo 40 > $ddrbw/window_ms
     done
 
+    for latfloor in $bus_dcvs/*/*latfloor
+    do
+	    echo 25000 > $latfloor/ipm_ceil
+    done
 
-    # enable LPMs for waipio
-    echo  N  >  /sys/devices/system/cpu/qcom_lpm/parameters/sleep_disabled
+    for l3gold in $bus_dcvs/L3/*gold
+    do
+	    echo 4000 > $l3gold/ipm_ceil
+    done
+
+    for l3prime in $bus_dcvs/L3/*prime
+    do
+	    echo 20000 > $l3prime/ipm_ceil
+    done
+
+    for ddrprime in $bus_dcvs/DDR/*prime
+    do
+	    echo 25 > $ddrprime/freq_scale_pct
+	    echo 1881 > $ddrprime/freq_scale_limit_mhz
+    done
+
+    for qosgold in $bus_dcvs/DDRQOS/*gold
+    do
+	    echo 50 > $qosgold/ipm_ceil
+    done
+
+    if [ "$rev" == "1.0" ]; then
+	    echo Y > /sys/devices/system/cpu/qcom_lpm/parameters/sleep_disabled
+	    echo 1 > /sys/devices/system/cpu/cpu0/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu1/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu2/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu3/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu4/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu5/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu6/cpuidle/state1/disable
+	    echo 1 > /sys/devices/system/cpu/cpu7/cpuidle/state1/disable
+	    echo 0 > "/sys/devices/platform/hypervisor/hypervisor:qcom,gh-watchdog/wakeup_enable"
+    else
+	    echo N > /sys/devices/system/cpu/qcom_lpm/parameters/sleep_disabled
+    fi
+
+    echo s2idle > /sys/power/mem_sleep
 
     # enable autosleep
     echo mem > /sys/power/autosleep

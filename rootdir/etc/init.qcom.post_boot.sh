02f6cd7538d59908914c18dd3b33905b0f296ae7
ziqic <quic_ziqic_01@quicinc.com>
2023-05-31 10:36:23 +0800

    post_boot: fix parameter reference problem
    CPU cores definition is in configure_sa8155_sku_parameters
    and configure_sa8195_sku_parameters. But the CPU core parameters
    are used before them, which leads to a problem.
    
    Change-Id: If128d8eb8fb5cd4f5347720c10afe5de8802649b
    Signed-off-by: ziqic <quic_ziqic_01@quicinc.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -377,6 +377,9 @@ esac
 
 case "$target" in
     "sa8155p" |"sa8155" )
+    #configure_memory_parameters
+    configure_sa8155_sku_parameters
+
     # Core control parameters for gold
     echo 2 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/min_cpus
     echo 60 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_up_thres
@@ -537,8 +540,6 @@ case "$target" in
     done
 
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-    #configure_memory_parameters
-    configure_sa8155_sku_parameters
     ;;
 esac
 
@@ -587,6 +588,8 @@ esac
 
 case "$target" in
     "sa8195p" )
+     configure_sa8195_sku_parameters
+
      # Core control parameters for gold+
      echo 2 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/min_cpus
      echo 60 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_up_thres
@@ -639,7 +642,6 @@ case "$target" in
      else
          echo 1 > /proc/sys/vm/reap_mem_on_sigkill
      fi
-    configure_sa8195_sku_parameters
 esac
 
 case "$target" in

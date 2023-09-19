384cba415eb9880238ad4b2f54bfc31ce319a7c5
Keerthi Gowda <quic_kbalehal@quicinc.com>
2023-09-19 10:00:42 -0700

    postboot: Add support for trustedvm-v3
    Change-Id: I9cef9821de3afe5cdc608ef42f237d57ca920286

---
+++ rootdir/trustedvm-v3/init.post_boot.sh
@@ -0,0 +1,96 @@
+#! /bin/sh
+# Copyright (c) 2009-2021, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Changes from Qualcomm Innovation Center are provided under the following license:
+#
+# Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the
+# disclaimer below) provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+# GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+# HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+echo "4 4 1 4" > /proc/sys/kernel/printk
+sleep 6
+echo mem > /sys/power/autosleep
+
+if [ -f /etc/init.qti.debug.sh ]; then
+    /etc/init.qti.debug.sh
+fi
+
+echo -n "Started post boot settings " > /dev/kmsg
+
+#disable watermark boost
+echo 0 > /proc/sys/vm/watermark_boost_factor
+echo -n "Watermark boost is disabled" > /dev/kmsg
+
+#ftrace
+tracefs=/sys/kernel/debug/tracing
+
+#SPI
+mkdir $tracefs/instances/spi_qup
+echo 2 > $tracefs/instances/spi_qup/buffer_size_kb
+echo 1 > $tracefs/instances/spi_qup/events/qup_spi_trace/enable
+echo 1 > $tracefs/instances/spi_qup/tracing_on
+
+#I2C
+mkdir $tracefs/instances/i2c_qup
+echo 2 > $tracefs/instances/i2c_qup/buffer_size_kb
+echo 1 > $tracefs/instances/i2c_qup/events/qup_i2c_trace/enable
+echo 1 > $tracefs/instances/i2c_qup/tracing_on
+
+#GENI_COMMON
+#mkdir $tracefs/instances/qupv3_common
+#echo 2 > $tracefs/instances/qupv3_common/buffer_size_kb
+#echo 1 > $tracefs/instances/qupv3_common/events/qup_common_trace/enable
+#echo 1 > $tracefs/instances/qupv3_common/tracing_on

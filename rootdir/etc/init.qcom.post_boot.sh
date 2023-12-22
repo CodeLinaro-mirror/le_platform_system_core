32bc626e57d1d2687d3b34b8e0517de00d0a80ea
Madhu Ananthula <quic_mananthu@quicinc.com>
2023-12-22 10:10:55 +0530

    post_boot: Update scripts for SA8155 and SA8195
    Remove llcc & ddr specific configurations from post-boot scripts,
    as these are moved from post-boot script to the kernel drivers.
    
    Change-Id: Ife84c51fda892396f2c4874a9248a9fe07be4e54

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -384,66 +384,6 @@ esac
 
 case "$target" in
     "sa8155p" |"sa8155" )
-    #configure_memory_parameters
-    configure_sa8155_sku_parameters
-
-    # Core control parameters for gold
-    echo 2 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/min_cpus
-    echo 60 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_up_thres
-    echo 30 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_down_thres
-    echo 100 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/offline_delay_ms
-    echo 3 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/task_thres
-
-    # Core control parameters for gold+
-    echo 0 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/min_cpus
-    echo 60 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/busy_up_thres
-    echo 30 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/busy_down_thres
-    echo 100 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/offline_delay_ms
-    echo 1 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/task_thres
-    # Controls how many more tasks should be eligible to run on gold CPUs
-    # w.r.t number of gold CPUs available to trigger assist (max number of
-    # tasks eligible to run on previous cluster minus number of CPUs in
-    # the previous cluster).
-    #
-    # Setting to 1 by default which means there should be at least
-    # 4 tasks eligible to run on gold cluster (tasks running on gold cores
-    # plus misfit tasks on silver cores) to trigger assitance from gold+.
-    echo 1 > /sys/devices/system/cpu/cpu${prime_core}/core_ctl/nr_prev_assist_thresh
-
-    # Disable Core control on silver
-    echo 0 > /sys/devices/system/cpu/cpu${silver_core}/core_ctl/enable
-
-    # Setting b.L scheduler parameters
-    echo 95 95 > /proc/sys/kernel/sched_upmigrate
-    echo 85 85 > /proc/sys/kernel/sched_downmigrate
-    echo 100 > /proc/sys/kernel/sched_group_upmigrate
-    echo 10 > /proc/sys/kernel/sched_group_downmigrate
-    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
-    # Turn off scheduler boost at the end
-    echo 0 > /proc/sys/kernel/sched_boost
-
-    # configure governor settings for silver cluster
-    echo "schedutil" > $cpufreq_silver/scaling_governor
-    echo 0 > $cpufreq_silver/schedutil/up_rate_limit_us
-    echo 0 > $cpufreq_silver/schedutil/down_rate_limit_us
-    echo 1209600 > $cpufreq_silver/schedutil/hispeed_freq
-    echo 1 > $cpufreq_silver/schedutil/pl
-
-    # configure governor settings for gold cluster
-    echo "schedutil" > $cpufreq_gold/scaling_governor
-    echo 0 > $cpufreq_gold/schedutil/up_rate_limit_us
-    echo 0 > $cpufreq_gold/schedutil/down_rate_limit_us
-    echo 1612800 > $cpufreq_gold/schedutil/hispeed_freq
-    echo 1 > $cpufreq_gold/schedutil/pl
-
-    # configure governor settings for gold+ cluster
-    echo "schedutil" > $cpufreq_prime/scaling_governor
-    echo 0 > $cpufreq_prime/schedutil/up_rate_limit_us
-    echo 0 > $cpufreq_prime/schedutil/down_rate_limit_us
-    echo 1612800 > $cpufreq_prime/schedutil/hispeed_freq
-    echo 1 > $cpufreq_prime/schedutil/pl
-
     # configure input boost settings
     echo "0:1324800" > /sys/module/cpu_boost/parameters/input_boost_freq
     echo 120 > /sys/module/cpu_boost/parameters/input_boost_ms
@@ -459,93 +399,15 @@ case "$target" in
         echo 1 > /proc/sys/vm/reap_mem_on_sigkill
     fi
 
-    # Enable bus-dcvs
-    for device in /sys/devices/platform/soc
-    do
-        for cpubw in $device/*cpu-cpu-llcc-bw/devfreq/*cpu-cpu-llcc-bw
-        do
-        echo "bw_hwmon" > $cpubw/governor
-        echo 40 > $cpubw/polling_interval
-        echo "2288 4577 7110 9155 12298 14236 15258" > $cpubw/bw_hwmon/mbps_zones
-        echo 4 > $cpubw/bw_hwmon/sample_ms
-        echo 50 > $cpubw/bw_hwmon/io_percent
-        echo 20 > $cpubw/bw_hwmon/hist_memory
-        echo 10 > $cpubw/bw_hwmon/hyst_length
-        echo 30 > $cpubw/bw_hwmon/down_thres
-        echo 0 > $cpubw/bw_hwmon/guard_band_mbps
-        echo 250 > $cpubw/bw_hwmon/up_scale
-        echo 1600 > $cpubw/bw_hwmon/idle_mbps
-        echo 14236 > $cpubw/max_freq
-        done
-
-        for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
-        do
-        echo "bw_hwmon" > $llccbw/governor
-        echo 40 > $llccbw/polling_interval
-        echo "1720 2929 3879 5931 6881 7980" > $llccbw/bw_hwmon/mbps_zones
-        echo 4 > $llccbw/bw_hwmon/sample_ms
-        echo 80 > $llccbw/bw_hwmon/io_percent
-        echo 20 > $llccbw/bw_hwmon/hist_memory
-        echo 10 > $llccbw/bw_hwmon/hyst_length
-        echo 30 > $llccbw/bw_hwmon/down_thres
-        echo 0 > $llccbw/bw_hwmon/guard_band_mbps
-        echo 250 > $llccbw/bw_hwmon/up_scale
-        echo 1600 > $llccbw/bw_hwmon/idle_mbps
-        echo 6881 > $llccbw/max_freq
-        done
-
-        for npubw in $device/*npu-npu-ddr-bw/devfreq/*npu-npu-ddr-bw
-        do
-        echo 1 > /sys/devices/virtual/npu/msm_npu/pwr
-        echo "bw_hwmon" > $npubw/governor
-        echo 40 > $npubw/polling_interval
-        echo "1720 2929 3879 5931 6881 7980" > $npubw/bw_hwmon/mbps_zones
-        echo 4 > $npubw/bw_hwmon/sample_ms
-        echo 80 > $npubw/bw_hwmon/io_percent
-        echo 20 > $npubw/bw_hwmon/hist_memory
-        echo 6  > $npubw/bw_hwmon/hyst_length
-        echo 30 > $npubw/bw_hwmon/down_thres
-        echo 0 > $npubw/bw_hwmon/guard_band_mbps
-        echo 250 > $npubw/bw_hwmon/up_scale
-        echo 0 > $npubw/bw_hwmon/idle_mbps
-        echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
-        done
-
-        #Enable mem_latency governor for L3, LLCC, and DDR scaling
-        for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
-        do
-        echo "mem_latency" > $memlat/governor
-        echo 10 > $memlat/polling_interval
-        echo 400 > $memlat/mem_latency/ratio_ceil
-        done
-
-        #Enable userspace governor for L3 cdsp nodes
-        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
-        do
-        echo "cdspl3" > $l3cdsp/governor
-        done
-
-        #Enable compute governor for gold latfloor
-        for latfloor in $device/*cpu-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
-        do
-        echo "compute" > $latfloor/governor
-        echo 10 > $latfloor/polling_interval
-        done
-
-        #Gold L3 ratio ceil
-        for l3gold in $device/*cpu4-cpu-l3-lat/devfreq/*cpu4-cpu-l3-lat
-        do
-        echo 4000 > $l3gold/mem_latency/ratio_ceil
-        done
-
-        #Prime L3 ratio ceil
-        for l3prime in $device/*cpu7-cpu-l3-lat/devfreq/*cpu7-cpu-l3-lat
-        do
-        echo 20000 > $l3prime/mem_latency/ratio_ceil
-        done
-    done
-
     echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+    #configure_memory_parameters
+    configure_sa8155_sku_parameters
+    # configure governor settings for silver, gold and gold+ clusters
+    echo "schedutil" > $cpufreq_silver/scaling_governor
+    echo "schedutil" > $cpufreq_gold/scaling_governor
+    echo "schedutil" > $cpufreq_prime/scaling_governor
+    # Turn off scheduler boost at the end
+    echo 0 > /proc/sys/kernel/sched_boost
     ;;
 esac
 
@@ -595,42 +457,6 @@ esac
 
 case "$target" in
     "sa8195p" )
-     configure_sa8195_sku_parameters
-
-     # Core control parameters for gold+
-     echo 2 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/min_cpus
-     echo 60 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_up_thres
-     echo 30 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/busy_down_thres
-     echo 100 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/offline_delay_ms
-     echo 4 > /sys/devices/system/cpu/cpu${gold_core}/core_ctl/task_thres
-
-     # Disable Core control on silver
-     echo 0 > /sys/devices/system/cpu/cpu${silver_core}/core_ctl/enable
-
-     # Setting b.L scheduler parameters
-     echo 95 95 > /proc/sys/kernel/sched_upmigrate
-     echo 85 85 > /proc/sys/kernel/sched_downmigrate
-     echo 100 > /proc/sys/kernel/sched_group_upmigrate
-     echo 10 > /proc/sys/kernel/sched_group_downmigrate
-     echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
-     # Turn off scheduler boost at the end
-     echo 0 > /proc/sys/kernel/sched_boost
-
-     # configure governor settings for silver cluster
-     echo "schedutil" > $cpufreq_silver/scaling_governor
-     echo 0 > $cpufreq_silver/schedutil/up_rate_limit_us
-     echo 0 > $cpufreq_silver/schedutil/down_rate_limit_us
-     echo 1209600 > $cpufreq_silver/schedutil/hispeed_freq
-     echo 1 > $cpufreq_silver/schedutil/pl
-
-     # configure governor settings for gold+ cluster
-     echo "schedutil" > $cpufreq_gold/scaling_governor
-     echo 0 > $cpufreq_gold/schedutil/up_rate_limit_us
-     echo 0 > $cpufreq_gold/schedutil/down_rate_limit_us
-     echo 1612800 > $cpufreq_gold/schedutil/hispeed_freq
-     echo 1 > $cpufreq_gold/schedutil/pl
-
      # configure input boost settings
      echo "0:1324800" > /sys/module/cpu_boost/parameters/input_boost_freq
      echo 120 > /sys/module/cpu_boost/parameters/input_boost_ms
@@ -648,6 +474,12 @@ case "$target" in
      else
          echo 1 > /proc/sys/vm/reap_mem_on_sigkill
      fi
+     configure_sa8195_sku_parameters
+     # configure governor settings for silver and gold clusters
+     echo "schedutil" > $cpufreq_silver/scaling_governor
+     echo "schedutil" > $cpufreq_gold/scaling_governor
+     # Turn off scheduler boost at the end
+     echo 0 > /proc/sys/kernel/sched_boost
 ;;
 esac
 

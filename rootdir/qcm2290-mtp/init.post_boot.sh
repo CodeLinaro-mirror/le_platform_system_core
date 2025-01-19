959d43c127f405b903694bee24fd8535bae68a1a
Vishal Varma <quic_vishalpr@quicinc.com>
2025-01-19 22:23:35 +0530

    core: Remove duplicate soc_id in post boot script
    Removed duplicate soc_id in post boot script for
    qcm2290-mtp since they are already covered in
    second condition.
    
    Change-Id: I09c922035e226131fe4aaedf1a848ea6a0ca99a5
    Signed-off-by: Vishal Varma <quic_vishalpr@quicinc.com>

--- rootdir/qcm2290-mtp/init.post_boot.sh
+++ rootdir/qcm2290-mtp/init.post_boot.sh
@@ -214,7 +214,7 @@ echo $sched_rt_period_us > /proc/sys/kernel/sched_rt_period_us
 echo $sched_rt_runtime_us > /proc/sys/kernel/sched_rt_runtime_us
 
 case "$soc_id" in
-	"586" | "471" | "473" | "441")
+	"586")
 		# Disable Core control on silver
 		echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
 

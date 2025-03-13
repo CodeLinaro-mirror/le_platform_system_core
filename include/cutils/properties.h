9cd8c68d95cc5c6d9d21501f2148f0f3905cda0f
mxing <quic_mxing@quicinc.com>
2025-03-13 19:41:44 +0800

    system: core: leproperties: Fix compilation issue of leproperties
    After Upgrade GCC14 version, it report -Wimplicit-function-declaration
    errors. Using <sys/stat.h>, "ll.h" and function dump_properties
    declaration to fix this issue.
    
    Change-Id: Ic37db263d3b320d0bd06b062e45295e0e28f806a

--- include/cutils/properties.h
+++ include/cutils/properties.h
@@ -109,6 +109,7 @@ int32_t property_get_int32(const char *key, int32_t default_value);
 int property_set(const char *key, const char *value);
     
 int property_list(void (*propfn)(const char *key, const char *value, void *cookie), void *cookie);    
+void dump_properties(void);
 
 #if defined(__BIONIC_FORTIFY)
 

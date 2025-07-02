94f97e617bfe35ee15d41cf1097f20c38111d43b
Zhiyuan Chen <zhiyuanc@qti.qualcomm.com>
2025-07-02 17:32:48 +0800

    core: fix init_post_boot.service start failed
    use /bin/sh to execute the script
    
    Change-Id: I2dd595349a7a4e8b199646854c7a5213004f66e1

--- rootdir/sun/init.post_boot.sh
+++ rootdir/sun/init.post_boot.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #=============================================================================
 # Copyright (c) 2009-2012, 2014-2019, The Linux Foundation. All rights reserved.
 #
@@ -191,9 +192,3 @@ function configure_memory_parameters() {
 	fi
 }
 configure_memory_parameters
-#=============================================================================
-# Copyright (c) 2009-2012, 2014-2019, The Linux Foundation. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright

1693aed978cecdc35f97ad22a2390c5c3ca6bcfd
Baihui Zeng <baihzeng@qti.qualcomm.com>
2025-08-12 18:48:22 +0800

    fix kw issue
    Change-Id: I49dd1c563e077c96437988358bf81ab52fa71fb4
    Signed-off-by: Baihui Zeng <baihzeng@qti.qualcomm.com>

--- libcutils/fs.c
+++ libcutils/fs.c
@@ -156,7 +156,9 @@ int fs_mkdirs(const char* path, mode_t mode) {
     struct stat sb;
     char* buf = strdup(path);
 
-    if (buf != NULL && *buf != '/') {
+    if (buf == NULL)
+        return -ENOMEM;
+    if (*buf != '/') {
         ALOGE("Relative paths are not allowed: %s", buf);
         res = -EINVAL;
         goto done;

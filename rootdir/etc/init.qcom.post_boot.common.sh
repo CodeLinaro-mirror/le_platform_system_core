48a8e79137ed1618e20ed7b324689596f9baccaf
Venkatakrishnaiah Pari <vpari@qti.qualcomm.com>
2025-10-09 16:05:52 -0700

    post-boot: moved suspend_resume and gunyah trace to instance
    Moved suspend_resume and gunyah trace to separate
    trace instances buffer.
    
    Change-Id: Ic5833a973da6939c093e5c74c453820134f3f43f

--- rootdir/etc/init.qcom.post_boot.common.sh
+++ rootdir/etc/init.qcom.post_boot.common.sh
@@ -43,9 +43,7 @@ enable_debug_tracing_events() {
         workqueue \
         ipi \
         sched \
-        gunyah \
         oom \
-        power/suspend_resume \
     "
 
     for event in $events; do
@@ -55,7 +53,7 @@ enable_debug_tracing_events() {
      #Enable for seperate events tracing
 
     tracing_events_instance_dir="/sys/kernel/debug/tracing/instances/"
-    specific_events="safelinux rwmmio secure_buffer scmi"
+    specific_events="safelinux rwmmio secure_buffer gunyah scmi suspend_resume"
 
     for event in $specific_events; do
         instance="$tracing_events_instance_dir/$event"
